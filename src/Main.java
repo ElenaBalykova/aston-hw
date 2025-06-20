8797366f10cf33f611cedc7a3d379dcea258ae36
Elena Balykova <elenavorob3@gmail.com>
2025-06-20 22:32:54 +0300

    feat: add checkIntSign() method

--- src/Main.java
+++ src/Main.java
@@ -50,6 +50,13 @@ public class Main {
         }
         return false;
     }
+    public static void checkIntSign(int a){
+        if (a >= 0){
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+    }
 
     public static void main(String[] args) {
         printThreeWords();
@@ -57,5 +64,6 @@ public class Main {
         printColor();
         compareNumbers();
         System.out.println(checkIntSum(16, 4));
+        checkIntSign(0);
     }
 }
