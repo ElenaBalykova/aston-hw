7108cd9a0b5898a2efd8cc39129e23174344004f
Elena Balykova <elenavorob3@gmail.com>
2025-06-20 21:49:16 +0300

    feat: add checkSumSign() method

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
+    public static void checkSumSign() {
+        int a = -4;
+        int b = 3;
+        int sum = a + b;
+
+        if (sum >= 0){
+            System.out.println("Cумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println();
-        Main.printThreeWords();
+        printThreeWords();
+        checkSumSign();
     }
 }
