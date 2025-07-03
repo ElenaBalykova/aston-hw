80efe535cd2a2352e8826a9200c8e73189e5616f
Elena Balykova <elenavorob3@gmail.com>
2025-07-04 00:25:13 +0300

    feat: add exception handling

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.sql.Array;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public int arrayProc(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+        int currentRow = 0;
+        int currentCol = 0;
+        int sum = 0;
+
+        if (arr[0].getClass().isArray() && arr.length == 2) {
+            for (int i = 0; i < arr.length; i++) {
+                currentRow = i;
+                for (int j = 0; j < arr[currentRow].length; j++) {
+                    currentCol = j;
+                    try {
+                        sum += Integer.parseInt(arr[i][j]);
+                    } catch (NumberFormatException e) {
+                        throw new MyArrayDataException("Ошибка преобразования", currentRow, currentCol);
+                    }
+
+                }
+                System.out.println();
+            }
+        } else {
+            throw new MyArraySizeException("Массив не двумерный");
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
+        Main sumNum = new Main();
+        String arr[][] = {
+            {"1", "2", "3"},
+            {"4", "5", "6"},
+        };
+        System.out.println(sumNum.arrayProc(arr));
 
     }
 }
---
+++ src/MyArrayDataException.java
@@ -0,0 +1,10 @@
+public class MyArrayDataException extends Exception{
+    private final int row;
+    private final int col;
+
+    public MyArrayDataException(String message, int row, int col) {
+        super(message);
+        this.row = row;
+        this.col = col;
+    }
+}
